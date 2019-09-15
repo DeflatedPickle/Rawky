6ce2d81fc7c20845bf93b1605da3f346e946f032
DeflatedPickle <DeflatedPickle@gmail.com>
2019-09-15 22:19:22 +0100

    Update Settings.kt

--- src/main/kotlin/com/deflatedpickle/rawky/dialogue/Settings.kt
+++ src/main/kotlin/com/deflatedpickle/rawky/dialogue/Settings.kt
@@ -5,6 +5,7 @@ import com.bric.colorpicker.ColorPickerDialog
 import com.bulenkov.darcula.DarculaLaf
 import com.deflatedpickle.rawky.util.Components
 import org.pushingpixels.substance.api.skin.SubstanceGraphiteElectricLookAndFeel
+import java.awt.BorderLayout
 import java.awt.Dimension
 import java.awt.GridBagConstraints
 import java.awt.GridBagLayout
@@ -26,85 +27,80 @@ class Settings : JDialog(Components.frame, "Settings") {
         fill = GridBagConstraints.HORIZONTAL
     }
 
+    val node = DefaultMutableTreeNode().apply {
+        add(DefaultMutableTreeNode("Appearance"))
+        add(DefaultMutableTreeNode("Components").apply {
+            add(DefaultMutableTreeNode("Colour Library"))
+            add(DefaultMutableTreeNode("Colour Palette"))
+            add(DefaultMutableTreeNode("Colour Shades"))
+            add(DefaultMutableTreeNode("Layer List"))
+            add(DefaultMutableTreeNode("Pixel Grid"))
+            add(DefaultMutableTreeNode("Tiled View"))
+            add(DefaultMutableTreeNode("Toolbox"))
+        })
+    }
+
+    val tree = JTree(node).apply {
+        isRootVisible = false
+
+        addTreeSelectionListener {
+            panel.removeAll()
+
+            val pathTemp = mutableListOf<String>()
+            for (i in 0 until it.path.pathCount) {
+                pathTemp.add(it.path.getPathComponent(i).toString().replace(" ", "_"))
+            }
+
+            when (pathTemp.joinToString(">").toLowerCase()) {
+                ">appearance" -> {
+                    panel.add(JLabel("Look and Feel:"), labelConstraints)
+                    panel.add(JComboBox<String>(appearanceComboBoxModel).apply {
+                        addActionListener {
+                            when (selectedIndex) {
+                                0 -> UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName())
+                                1 -> UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+                                2 -> UIManager.setLookAndFeel(DarculaLaf())
+                                3 -> UIManager.setLookAndFeel(SubstanceGraphiteElectricLookAndFeel())
+                                4 -> UIManager.setLookAndFeel(WebLookAndFeel())
+                            }
+                            SwingUtilities.updateComponentTreeUI(Components.frame)
+                            SwingUtilities.updateComponentTreeUI(this@Settings)
+                        }
+                    }, lineEnd)
+                }
+                ">components>pixel_grid" -> {
+                    panel.add(JLabel("Pixel Smooth:"), labelConstraints)
+                    panel.add(JSlider(0, 200, Components.pixelGrid.pixelSmooth).apply { addChangeListener { Components.pixelGrid.pixelSmooth = this.value } }, lineEnd)
+
+                    panel.add(JLabel("Background Fill Even:"), labelConstraints)
+                    panel.add(JButton().apply { addActionListener { Components.pixelGrid.backgroundFillEven = ColorPickerDialog.showDialog(this@Settings, Components.pixelGrid.backgroundFillEven) } }, lineEnd)
+
+                    panel.add(JLabel("Background Fill Odd:"), labelConstraints)
+                    panel.add(JButton().apply { addActionListener { Components.pixelGrid.backgroundFillOdd = ColorPickerDialog.showDialog(this@Settings, Components.pixelGrid.backgroundFillOdd) } }, lineEnd)
+
+                    panel.add(JLabel("Hover Opacity:"), labelConstraints)
+                    panel.add(JSlider(1, 255, Components.pixelGrid.hoverOpacity).apply { addChangeListener { Components.pixelGrid.hoverOpacity = this.value } }, lineEnd)
+
+                    panel.add(JLabel("Line Thickness:"), labelConstraints)
+                    panel.add(JSlider(1, 200, Components.pixelGrid.lineThickness.toInt()).apply { addChangeListener { Components.pixelGrid.lineThickness = (this.value / 10).toFloat() } }, lineEnd)
+
+                    panel.add(JLabel("Grid Colour:"), labelConstraints)
+                    panel.add(JButton().apply { addActionListener { Components.pixelGrid.gridColour = ColorPickerDialog.showDialog(this@Settings, Components.pixelGrid.gridColour) } }, lineEnd)
+                }
+            }
+            panel.revalidate()
+            panel.repaint()
+        }
+    }
+
     val appearanceComboBoxModel = DefaultComboBoxModel(arrayOf("Metal", "System", "Dracula", "Substance Graphite Electric", "Web"))
 
     init {
-        layout = GridBagLayout()
+        layout = BorderLayout()
         size = Dimension(600, 400)
 
-        val node = DefaultMutableTreeNode().apply {
-            add(DefaultMutableTreeNode("Appearance"))
-            add(DefaultMutableTreeNode("Components").apply {
-                add(DefaultMutableTreeNode("Colour Library"))
-                add(DefaultMutableTreeNode("Colour Palette"))
-                add(DefaultMutableTreeNode("Colour Shades"))
-                add(DefaultMutableTreeNode("Layer List"))
-                add(DefaultMutableTreeNode("Pixel Grid"))
-                add(DefaultMutableTreeNode("Tiled View"))
-                add(DefaultMutableTreeNode("Toolbox"))
-            })
-        }
-
-        add(JScrollPane(JTree(node).apply {
-            isRootVisible = false
-
-            addTreeSelectionListener {
-                panel.removeAll()
-
-                val pathTemp = mutableListOf<String>()
-                for (i in 0 until it.path.pathCount) {
-                    pathTemp.add(it.path.getPathComponent(i).toString().replace(" ", "_"))
-                }
-
-                when (pathTemp.joinToString(">").toLowerCase()) {
-                    ">appearance" -> {
-                        panel.add(JLabel("Look and Feel:"), labelConstraints)
-                        panel.add(JComboBox<String>(appearanceComboBoxModel).apply {
-                            addActionListener {
-                                when (selectedIndex) {
-                                    0 -> UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName())
-                                    1 -> UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
-                                    2 -> UIManager.setLookAndFeel(DarculaLaf())
-                                    3 -> UIManager.setLookAndFeel(SubstanceGraphiteElectricLookAndFeel())
-                                    4 -> UIManager.setLookAndFeel(WebLookAndFeel())
-                                }
-                                SwingUtilities.updateComponentTreeUI(Components.frame)
-                                SwingUtilities.updateComponentTreeUI(this@Settings)
-                            }
-                        }, lineEnd)
-                    }
-                    ">components>pixel_grid" -> {
-                        panel.add(JLabel("Pixel Smooth:"), labelConstraints)
-                        panel.add(JSlider(0, 200, Components.pixelGrid.pixelSmooth).apply { addChangeListener { Components.pixelGrid.pixelSmooth = this.value } }, lineEnd)
-
-                        panel.add(JLabel("Background Fill Even:"), labelConstraints)
-                        panel.add(JButton().apply { addActionListener { Components.pixelGrid.backgroundFillEven = ColorPickerDialog.showDialog(this@Settings, Components.pixelGrid.backgroundFillEven) } }, lineEnd)
-
-                        panel.add(JLabel("Background Fill Odd:"), labelConstraints)
-                        panel.add(JButton().apply { addActionListener { Components.pixelGrid.backgroundFillOdd = ColorPickerDialog.showDialog(this@Settings, Components.pixelGrid.backgroundFillOdd) } }, lineEnd)
-
-                        panel.add(JLabel("Hover Opacity:"), labelConstraints)
-                        panel.add(JSlider(1, 255, Components.pixelGrid.hoverOpacity).apply { addChangeListener { Components.pixelGrid.hoverOpacity = this.value } }, lineEnd)
-
-                        panel.add(JLabel("Line Thickness:"), labelConstraints)
-                        panel.add(JSlider(1, 200, Components.pixelGrid.lineThickness.toInt()).apply { addChangeListener { Components.pixelGrid.lineThickness = (this.value / 10).toFloat() } }, lineEnd)
-
-                        panel.add(JLabel("Grid Colour:"), labelConstraints)
-                        panel.add(JButton().apply { addActionListener { Components.pixelGrid.gridColour = ColorPickerDialog.showDialog(this@Settings, Components.pixelGrid.gridColour) } }, lineEnd)
-                    }
-                }
-                panel.revalidate()
-                panel.repaint()
-            }
-        }), GridBagConstraints().apply {
-            weightx = 0.0
-            weighty = 1.0
-            fill = GridBagConstraints.VERTICAL
-        })
-        add(JScrollPane(panel), GridBagConstraints().apply {
-            weightx = 1.0
-            weighty = 1.0
-            fill = GridBagConstraints.BOTH
+        add(JSplitPane(JSplitPane.HORIZONTAL_SPLIT, JScrollPane(tree), JScrollPane(panel)).apply {
+            dividerLocation = 150
         })
     }
 }
