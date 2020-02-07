c2f51fb5ba22dcdbb2f3b6de5a47766c496f9577
DeflatedPickle <DeflatedPickle@gmail.com>
2020-02-07 12:51:47 +0000

    Update Stipple.kt

--- src/main/kotlin/com/deflatedpickle/rawky/tool/fill/Stipple.kt
+++ src/main/kotlin/com/deflatedpickle/rawky/tool/fill/Stipple.kt
@@ -3,6 +3,7 @@
 package com.deflatedpickle.rawky.tool.fill
 
 import com.deflatedpickle.rawky.api.annotations.Colour
+import com.deflatedpickle.rawky.api.annotations.IntOpt
 import com.deflatedpickle.rawky.api.annotations.Options
 import com.deflatedpickle.rawky.api.annotations.Tooltip
 import com.deflatedpickle.rawky.component.PixelGrid
@@ -15,10 +16,20 @@ object Stipple : Fill {
         @Tooltip("The colour to use for the alternate colour")
         @JvmField
         var altColour: Color = PixelGrid.defaultColour()
+
+        @IntOpt(1, 16)
+        @Tooltip("The value to divide the row by, then use the remainder of")
+        @JvmField
+        var modulusRow = 2
+
+        @IntOpt(1, 16)
+        @Tooltip("The value to divide the column by, then use the remainder of")
+        @JvmField
+        var modulusColumn = 2
     }
 
     override fun perform(cell: PixelGrid.Cell, row: Int, column: Int, shade: Color) {
-        if (row % 2 == column % 2) {
+        if (row % Settings.modulusRow == column % Settings.modulusColumn) {
             cell.colour = shade
         } else {
             if (Settings.altColour != PixelGrid.defaultColour()) {
