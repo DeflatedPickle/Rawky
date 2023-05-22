589288ec8650e544d7c7a566f1da2476edc442f0
DeflatedPickle <deflatedpickle@gmail.com>
2023-05-22 13:40:48 +0100

    Fixed errors when opening some settings pages

--- colourhistory/src/main/kotlin/com/deflatedpickle/rawky/colourhistory/ColourHistorySettings.kt
+++ colourhistory/src/main/kotlin/com/deflatedpickle/rawky/colourhistory/ColourHistorySettings.kt
@@ -5,6 +5,7 @@
 package com.deflatedpickle.rawky.colourhistory
 
 import com.deflatedpickle.haruhi.api.config.Config
+import com.deflatedpickle.rawky.settings.api.range.IntRange
 import com.deflatedpickle.undulation.serializer.ColorSerializer
 import kotlinx.serialization.Serializable
 import java.awt.Color
@@ -13,5 +14,5 @@ import java.awt.Color
 data class ColourHistorySettings(
     override val version: Int = 1,
     val history: MutableList<@Serializable(ColorSerializer::class) Color> = mutableListOf(),
-    var historyLength: Int = 24,
+    @IntRange(1, 100) var historyLength: Int = 24,
 ) : Config
